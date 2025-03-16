aeecebd8dae351c8d1036c5527ef72910e0d8167
MikeHill89 <MikeHill89@no-reply.com>
2025-03-16 15:00:23 +0200

    Fixed assets incorrectly loading from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -32,6 +31,16 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: "asset/resource",
+                generator: {
+                    filename: "images/[name][ext]", // Puts images in dist/images
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "fonts/[name][ext]", // Puts fonts in dist/fonts
+                },
             },
         ],
     },
